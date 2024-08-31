74be329d6435d3bc65b8ccc86f4f9758cf8b2a45
noyon674 <sarker35-674@diu.edu.bd>
2024-08-31 07:28:08 +0600

    initialaze all the data types
    - implement all the data

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args){
+        // Data types: In java, there is two types of data. primitive and non-primitive
+        // primitive data are int, String, boolean, float, double
+        // non-primitive data is objects
+        String name = "Noyon Sarker";
+        int marks = 80;
+        float cgpa = 4.50f;
+        boolean isLoggedIn = true;
+        double point = 25.30d;
+        System.out.println("Name: "+name);
+        System.out.println("Marks: "+marks);
+        System.out.println("CGPA: "+cgpa);
+        System.out.println("Boolean: "+isLoggedIn);
+        System.out.println("Point: "+point);
+    }
+}
