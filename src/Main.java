af9d6f0c6737d5c6b50b0c5615ff3f157d1ff997
noyon674 <sarker35-674@diu.edu.bd>
2024-09-01 12:53:49 +0600

    added 2D array

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        // Data types: In java, there is two types of data. primitive and non-primitive
-        // primitive data are int, String, boolean, float, double
-        // non-primitive data is objects
-        String name = "Noyon Sarker";
-        int marks = 80;
-        float cgpa = 4.50f;
-        boolean isLoggedIn = true;
-        double point = 25.30d;
-        System.out.println("Name: "+name);
-        System.out.println("Marks: "+marks);
-        System.out.println("CGPA: "+cgpa);
-        System.out.println("Boolean: "+isLoggedIn);
-        System.out.println("Point: "+point);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter row number: ");
+        int row = sc.nextInt();
+        System.out.print("Enter col number: ");
+        int col = sc.nextInt();
+
+        int[][] arr = new int[row][col];
+
+
+        for (int i=0; i<row; i++){
+            for (int j=0; j<col; j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.print("Seach a number: ");
+        int num = sc.nextInt();
+
+        for (int i=0; i<row; i++){
+            for (int j=0; j<col; j++){
+                if (arr[i][j]==num){
+                    System.out.println(i+" "+j);
+                }
+            }
+            System.out.println();
+        }
     }
 }
