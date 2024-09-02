0e7364d262ba838a52a5631e8800c4a70552f6ba
noyon674 <developernoyon9@gmail.com>
2024-09-02 18:56:35 +0600

    added linear search code

--- src/Main.java
+++ src/Main.java
@@ -2,35 +2,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter row: ");
-        int row = sc.nextInt();
-        System.out.print("Enter column: ");
-        int col = sc.nextInt();
+       int []arr = {10,34,5,68,57,4,7,8};
 
-        int [][]arr = new int[row][col];
+       int num = 5;
 
-        for (int i=0; i<row; i++){
-            for (int j=0; j<col; j++){
-                arr[i][j] = sc.nextInt();
-            }
-        }
-
-        //print arr
-        for (int i=0; i<row; i++){
-            for (int j=0; j<col; j++){
-                System.out.print(arr[i][j]+"  ");
-            }
-            System.out.println();
-        }
-        System.out.print("Search a number");
-        int num = sc.nextInt();
-        for (int i=0; i<row; i++){
-            for (int j=0; j<col; j++){
-                if (arr[i][j] == num){
-                    System.out.println("Positin of this number is: "+(i+1)+" "+(j+1));
-                }
-            }
-        }
+       for (int i=0; i<arr.length; i++){
+           if (num == arr[i]){
+               System.out.println(i);
+           }
+       }
     }
 }
