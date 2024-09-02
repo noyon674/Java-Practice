24a0c5f8a79ef0ae6056d5dc9d2e3097f3ba793f
noyon674 <sarker35-674@diu.edu.bd>
2024-09-02 18:25:26 +0600

    practice 2D array

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter row number: ");
+        System.out.print("Enter row: ");
         int row = sc.nextInt();
-        System.out.print("Enter col number: ");
+        System.out.print("Enter column: ");
         int col = sc.nextInt();
 
-        int[][] arr = new int[row][col];
-
+        int [][]arr = new int[row][col];
 
         for (int i=0; i<row; i++){
             for (int j=0; j<col; j++){
@@ -17,16 +16,21 @@ public class Main {
             }
         }
 
-        System.out.print("Seach a number: ");
-        int num = sc.nextInt();
-
+        //print arr
         for (int i=0; i<row; i++){
             for (int j=0; j<col; j++){
-                if (arr[i][j]==num){
-                    System.out.println(i+" "+j);
-                }
+                System.out.print(arr[i][j]+"  ");
             }
             System.out.println();
+        }
+        System.out.print("Search a number");
+        int num = sc.nextInt();
+        for (int i=0; i<row; i++){
+            for (int j=0; j<col; j++){
+                if (arr[i][j] == num){
+                    System.out.println("Positin of this number is: "+(i+1)+" "+(j+1));
+                }
+            }
         }
     }
 }
