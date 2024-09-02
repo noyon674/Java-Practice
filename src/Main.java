966833132b8d774b2e78b6d903b0b02bdf413ffb
noyon674 <developernoyon9@gmail.com>
2024-09-02 19:02:58 +0600

    added 2D array code

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-       int []arr = {10,34,5,68,57,4,7,8};
+       Scanner sc = new Scanner(System.in);
+        System.out.print("Enter row: ");
+        int row = sc.nextInt();
 
-       int num = 5;
+        System.out.print("Enter Col: ");
+        int col = sc.nextInt();
 
-       for (int i=0; i<arr.length; i++){
-           if (num == arr[i]){
-               System.out.println(i);
-           }
-       }
+        int [][] arr = new int[row][col];
+
+        for (int i=0; i<row; i++){
+            for (int j=0; j<col; j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i=0; i<row; i++){
+            for (int j=0; j<col; j++){
+                System.out.print(arr[i][j]+"  ");
+            }
+            System.out.println();
+        }
     }
 }
