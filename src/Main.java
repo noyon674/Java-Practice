3f17a0da9411f8a2db158a4cb6e73864b3414ade
noyon674 <developernoyon9@gmail.com>
2024-09-03 08:44:38 +0600

    practice string basic

--- src/Main.java
+++ src/Main.java
@@ -2,37 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-       Scanner sc = new Scanner(System.in);
-        System.out.print("Enter row: ");
-        int row = sc.nextInt();
+        String email = "sarker1w4tu2@gamil.com";
+        String userName = "";
+        int length = 0;
 
-        System.out.print("Enter Col: ");
-        int col = sc.nextInt();
-
-        int [][] arr = new int[row][col];
-
-        for (int i=0; i<row; i++){
-            for (int j=0; j<col; j++){
-                arr[i][j] = sc.nextInt();
-            }
+        for (int i=0; i<email.length(); i++){
+            if (email.charAt(i) == '@'){
+                break;
+            }else length +=1;
         }
 
-        for (int i=0; i<row; i++){
-            for (int j=0; j<col; j++){
-                System.out.print(arr[i][j]+"  ");
-            }
-            System.out.println();
-        }
-
-        System.out.print("Search a number from above array: ");
-        int num = sc.nextInt();
-
-        for (int i=0; i<row; i++){
-            for (int j=0; j<col; j++){
-                if (arr[i][j] == num){
-                    System.out.println("Positon of the number is: "+(i+1)+" "+(j+1));
-                }
-            }
-        }
+        System.out.println(email.substring(0, length));
     }
 }
