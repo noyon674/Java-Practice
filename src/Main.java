53a33b47d70339702b20afdbcac4c3447e397a4b
noyon674 <developernoyon9@gmail.com>
2024-09-14 17:49:16 +0600

    bouble sort practicing done

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
 import java.util.Scanner;
 
 public class Main {
+    public static void printArr (int []arrr){
+        for (int i=0; i<arrr.length; i++){
+            System.out.print(arrr[i]+" ");
+        }
+    }
     public static void main (String[] args){
-        int []arr = {7, 8, 3, 1, 2};
+        int [] arr = {10,3,6,3,29,4, 56, 3,46, 35};
 
-        for (int i=0; i<=arr.length - 1; i++){
-            for (int j=0; j<arr.length-i-1; j++){
+        for (int i=0; i<arr.length - 1; i++){
+            for (int j=0; j<arr.length - 1 - i; j++){
                 if(arr[j] > arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
@@ -13,8 +18,7 @@ public class Main {
                 }
             }
         }
-        for (int i=0; i<arr.length; i++){
-            System.out.print(arr[i]+" ");
-        }
+        printArr(arr);
+
     }
 }
