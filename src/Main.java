6d02bd1721fa025851d3b3c74bb72149f3cc5ed9
noyon674 <developernoyon9@gmail.com>
2024-09-14 18:19:38 +0600

    selection sort done

--- src/Main.java
+++ src/Main.java
@@ -7,16 +7,19 @@ public class Main {
         }
     }
     public static void main (String[] args){
-        int [] arr = {10,3,6,3,29,4, 56, 3,46, 35};
+        int [] arr = {10,3,35};
 
-        for (int i=0; i<arr.length - 1; i++){
-            for (int j=0; j<arr.length - 1 - i; j++){
-                if(arr[j] > arr[j+1]){
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+        for (int i=0; i<arr.length-1; i++){
+            int smallest = i;
+            for (int j=i ; j<arr.length-1; j++){
+                if (arr[smallest] > arr[j]){
+                    smallest = j;
                 }
             }
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+
         }
         printArr(arr);
 
