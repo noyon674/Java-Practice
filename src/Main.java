f6c3df49fe075330a70779f7ef2ad6608dbc1fc7
noyon674 <developernoyon9@gmail.com>
2024-09-20 23:49:33 +0600

    added scanner

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,11 @@
-import java.util.Scanner;
-
+import java.util.*;
 public class Main {
     public static void main (String[] args){
-        boolean isLoggedIn = true;
-        int age = 24;
-        String name = "Noyon";
-        float cgpa = 3.50f;
-        double Salary = 25000.50d;
-        char word = 'A';
+       Scanner sc = new Scanner(System.in); //scanner object
 
-        //normal way for print value
-        System.out.println("The booolean value is "+isLoggedIn);
+       int num = sc.nextInt(); //input integer
 
-        //format specifier for print  value
-        System.out.printf("The boolean value is %b", isLoggedIn);
-        System.out.printf("The intger value is %d\n", age);
-        System.out.printf("The string value is %s\n", name);
-        System.out.printf("The float value is %.2f\n", cgpa); //.2 for after floating point
-        System.out.printf("The double value is %.2f\n", Salary);
-        System.out.printf("The word value is %c\n", word);
+        System.out.println(num);
+
     }
 }
