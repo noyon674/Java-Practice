27ac63c46caf3449c694fdc4b7a2da4c9fe03219
noyon674 <developernoyon9@gmail.com>
2024-09-20 23:36:59 +0600

    format specifier

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,23 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void printArray(int ar[]){
-        for (int i=0; i<ar.length; i++){
-            System.out.print(ar[i]+" ");
-        }
-    }
     public static void main (String[] args){
-        //unsorted array
-        int []arr = {3,5,38,54,6};
-        //suppose 1st element is sorted, after all the unsorted arr
-        //unsorted array
-       for (int i=1; i<arr.length; i++){
-           int currentElement = arr[i];
-           int j = i-1;
-           while (j >= 0 && currentElement <arr[j]){
-               arr[j+1] = arr[j];
-               j--;
-           }
-           //placement
-           arr[j+1] = currentElement;
-       }
-        printArray(arr);
+        boolean isLoggedIn = true;
+        int age = 24;
+        String name = "Noyon";
+        float cgpa = 3.50f;
+        double Salary = 25000.50d;
+        char word = 'A';
+
+        //normal way for print value
+        System.out.println("The booolean value is "+isLoggedIn);
+
+        //format specifier for print  value
+        System.out.printf("The boolean value is %b", isLoggedIn);
+        System.out.printf("The intger value is %d\n", age);
+        System.out.printf("The string value is %s\n", name);
+        System.out.printf("The float value is %.2f\n", cgpa); //.2 for after floating point
+        System.out.printf("The double value is %.2f\n", Salary);
+        System.out.printf("The word value is %c\n", word);
     }
 }
