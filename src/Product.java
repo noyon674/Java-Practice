ef0a29b84a7302c8702e08172b1e7c11f36a0704
noyon674 <developernoyon9@gmail.com>
2024-09-21 00:22:09 +0600

    added vowel consonant checking code

---
+++ src/Product.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Product {
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().charAt(0);
+
+        if(ch =='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+            System.out.println("vowel");
+        }else System.out.println("Consonant");
+    }
+}
