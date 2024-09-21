f58a4394f1d1491efda254d0e87926b70aafc92e
noyon674 <developernoyon9@gmail.com>
2024-09-21 18:34:51 +0600

    added fibonacci sequence

--- src/Product.java
+++ src/Product.java
@@ -3,10 +3,16 @@ import java.util.Scanner;
 public class Product {
     public static void main(String []args){
         Scanner sc = new Scanner(System.in);
-        char ch = sc.next().charAt(0);
+        int n=sc.nextInt();
+        int f=0, s=1;
 
-        if(ch =='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
-            System.out.println("vowel");
-        }else System.out.println("Consonant");
+        for (int i=0; i<n; i++){
+            if(i>=2){
+                int t=f+s;
+                System.out.println(t);
+                f=s;
+                s=t;
+            }else System.out.println(i);
+        }
     }
 }
